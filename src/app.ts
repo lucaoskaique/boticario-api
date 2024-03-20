ecae379bad0c7f44cf57fa2a758e578266017e76
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-20 18:20:19 -0300

    Atualizações no código: importações corrigidas, rota principal modificada para retornar um JSON com a mensagem "Hello World DEV EM DOBRO", e mensagem de log do servidor atualizada para exibir a porta do ambiente.

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import 'dotenv/config'
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World DEV EM DOBRO" });
 });
 
 export { app };
