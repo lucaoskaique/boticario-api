706bdcf622e2e557ec602902baa0b1df0e162fe8
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-20 22:34:09 -0300

    Adiciona migração para criar tabela de endereços

---
+++ src/typeorm/migrations/1710984698447-CreateAddresses.ts
@@ -0,0 +1,60 @@
+import { type MigrationInterface, type QueryRunner, Table } from 'typeorm';
+
+export class CreateAddresses1710966775074 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'addresses',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'cep',
+            type: 'varchar',
+          },
+          {
+            name: 'street',
+            type: 'varchar',
+          },
+          {
+            name: 'neighborhood',
+            type: 'varchar',
+          },
+          {
+            name: 'city',
+            type: 'varchar',
+          },
+          {
+            name: 'street_number',
+            type: 'number',
+          },
+          {
+            name: 'complement',
+            type: 'varchar',
+          },
+          {
+            name: 'uf',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('addresses');
+  }
+}
