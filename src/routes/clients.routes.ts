f7f30093bfc0c1693aa4cb7d9e089658cd9a225b
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-21 18:17:17 -0300

    Adiciona rota para listar clientes

--- src/routes/clients.routes.ts
+++ src/routes/clients.routes.ts
@@ -1,10 +1,13 @@
 import { CreateClientController } from '@modules/clients/useCases/createClient/CreateClientController';
+import { ListClientsController } from '@modules/clients/useCases/listClients/ListClientsController';
 import { Router } from 'express';
 
 const clientsRoutes = Router();
 
 const createClientController = new CreateClientController();
+const listClientsController = new ListClientsController();
 
 clientsRoutes.post('/', createClientController.handle);
+clientsRoutes.get('/list', listClientsController.handle);
 
 export { clientsRoutes };
