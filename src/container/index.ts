568a2bf6c7786d9d5afbb0d1874ba584aabc84d3
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-21 18:00:59 -0300

    Adiciona repositório de endereços ao contêiner de injeção de dependência

--- src/container/index.ts
+++ src/container/index.ts
@@ -1,8 +1,15 @@
+import { AddressesRepository } from '@modules/clients/infra/typeorm/repositories/AddressesRepository';
 import { ClientsRepository } from '@modules/clients/infra/typeorm/repositories/ClientsRepository';
+import { type IAddressesRepository } from '@modules/clients/repositories/IAddressesRepository';
 import { type IClientsRepository } from '@modules/clients/repositories/IClientsRepository';
 import { container } from 'tsyringe';
 
 container.registerSingleton<IClientsRepository>(
   'ClientsRepository',
   ClientsRepository,
+);
+
+container.registerSingleton<IAddressesRepository>(
+  'AddressesRepository',
+  AddressesRepository,
 );
