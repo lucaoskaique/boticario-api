fa8dd1e0617aea64a4ed1b094b5a7e5644ea858d
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-21 18:01:42 -0300

    Adiciona controlador e caso de uso para criar endereço

---
+++ src/modules/clients/useCases/createAddress/CreateAddressController.ts
@@ -0,0 +1,44 @@
+import { type Request, type Response } from 'express';
+import { container } from 'tsyringe';
+import { z } from 'zod';
+
+import { CreateAddressUseCase } from './CreateAddressUseCase';
+
+class CreateAddressController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const createAddressBodySchema = z.object({
+      cep: z.string(),
+      street: z.string(),
+      street_number: z.number(),
+      complement: z.string(),
+      neighborhood: z.string(),
+      city: z.string(),
+      uf: z.string(),
+    });
+
+    const { cep, city, complement, neighborhood, street, street_number, uf } =
+      createAddressBodySchema.parse(request.body);
+
+    try {
+      const createAddressUseCase = container.resolve(CreateAddressUseCase);
+
+      await createAddressUseCase.execute({
+        cep,
+        city,
+        complement,
+        neighborhood,
+        street,
+        street_number,
+        uf,
+      });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.log(error);
+        return response.status(500).json({ errorMessage: error.message });
+      }
+    }
+    return response.status(201).send();
+  }
+}
+
+export { CreateAddressController };
---
+++ src/modules/clients/useCases/createAddress/CreateAddressUseCase.ts
@@ -0,0 +1,34 @@
+import { AppError } from '@errors/AppError';
+import { type ICreateAddressDTO } from '@modules/clients/dtos/ICreateAddressDTO';
+import { IAddressesRepository } from '@modules/clients/repositories/IAddressesRepository';
+import { inject, injectable } from 'tsyringe';
+
+@injectable()
+class CreateAddressUseCase {
+  constructor(
+    @inject('AddressesRepository')
+    private addressesRepository: IAddressesRepository,
+  ) {}
+
+  async execute({
+    cep,
+    street,
+    street_number,
+    complement,
+    neighborhood,
+    city,
+    uf,
+  }: ICreateAddressDTO): Promise<void> {
+    await this.addressesRepository.create({
+      cep,
+      street,
+      street_number,
+      complement,
+      neighborhood,
+      city,
+      uf,
+    });
+  }
+}
+
+export { CreateAddressUseCase };
