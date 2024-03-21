26b5c5b1063f1f9f8beb2027f073917363aac0e3
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-21 18:01:10 -0300

    Adiciona tratamento de erros e validação de dados

--- src/middlewares/errors.ts
+++ src/middlewares/errors.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { AppError } from '@errors/AppError';
+import { env } from 'env';
 import { type NextFunction, type Request, type Response } from 'express';
+import { ZodError } from 'zod';
 
 export const errorHandler = (
   err: Error,
@@ -8,11 +10,21 @@ export const errorHandler = (
   res: Response,
   next: NextFunction,
 ) => {
+  if (err instanceof ZodError) {
+    return res
+      .status(400)
+      .send({ message: 'Validation error.', issues: err.format() });
+  }
+
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({ message: err.message });
   }
 
-  console.error(err);
+  if (env.NODE_ENV !== 'production') {
+    console.error(err);
+  } else {
+    // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
+  }
 
   return res.status(500).json({
     status: 'error',
