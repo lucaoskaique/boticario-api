7ac8d087076e330abcd3d13576024e378b1b9bfa
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-20 22:54:08 -0300

    Adiciona arquivos de migração para criação de clientes, categorias e endereços

---
+++ src/typeorm/migrations/index.ts
@@ -0,0 +1,9 @@
+import { CreateClients1710984645200 } from './1710984645200-CreateClients';
+import { CreateCategories1710967133852 } from './1710984724753-CreateCategories';
+import { CreateAddresses1710985056783 } from './1710985056783-CreateAddresses';
+
+export {
+  CreateClients1710984645200,
+  CreateCategories1710967133852,
+  CreateAddresses1710985056783,
+};
