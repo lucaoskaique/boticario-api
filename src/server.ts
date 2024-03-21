9a376e0c97ddd2437149d40dc68545f731262ec7
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-20 22:24:14 -0300

    Adiciona inicialização do banco de dados no servidor

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,15 @@
+import { AppDataSource } from 'typeorm/data-source';
+
 import { app } from './app';
 import { env } from './env';
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Database connected');
+  })
+  .catch((error) => {
+    console.log('Database connection error', error);
+  });
 
 app.listen(env.PORT, () => {
   console.log(`🚀 Server started on port ${env.PORT}`);
