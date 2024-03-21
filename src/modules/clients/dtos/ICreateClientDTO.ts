626b32f8f9a9440e7db571b5a541f10852ea71b9
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-21 00:13:15 -0300

    Adiciona interface ICreateClientDTO para criação de clientes

---
+++ src/modules/clients/dtos/ICreateClientDTO.ts
@@ -0,0 +1,11 @@
+interface ICreateClientDTO {
+  username: string;
+  email: string;
+  password: string;
+  cpf: string;
+  phone: string;
+  birth_date: Date;
+  address_id?: string;
+}
+
+export type { ICreateClientDTO };
