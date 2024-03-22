f55da3924bc98c39aef44dd19a24fef65fe7e7fd
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-22 15:23:09 -0300

    Adiciona caso de uso para listar produtos

---
+++ src/modules/products/useCases/listProducts/ListProductsUseCase.ts
@@ -0,0 +1,18 @@
+import { type Product } from '@modules/products/infra/typeorm/entities/Product';
+import { IProductsRepository } from '@modules/products/repositories/IProductsRepository';
+import { inject, injectable } from 'tsyringe';
+
+@injectable()
+class ListProductsUseCase {
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
+
+  async execute(): Promise<Product[]> {
+    const products = await this.productsRepository.list();
+    return products;
+  }
+}
+
+export { ListProductsUseCase };
