645e91f1dcf6e13e0399849fff46e463bf2f076b
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-22 20:46:22 -0300

    Adicionada função de atualização de endereço na interface IAddressesRepository

--- src/modules/clients/repositories/IAddressesRepository.ts
+++ src/modules/clients/repositories/IAddressesRepository.ts
@@ -4,6 +4,7 @@ import { type Address } from '../infra/typeorm/entities/Address';
 interface IAddressesRepository {
   create: (data: ICreateAddressDTO) => Promise<void>;
   list: () => Promise<Address[]> | [];
+  update: (data: Address) => Promise<Address>;
 }
 
 export type { IAddressesRepository };
