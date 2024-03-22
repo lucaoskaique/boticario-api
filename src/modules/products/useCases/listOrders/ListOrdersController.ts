254b6f883bbb04e110958c7e3c1daa73d2ab67c9
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-22 17:39:12 -0300

    Adiciona ListOrdersController para listar pedidos

---
+++ src/modules/products/useCases/listOrders/ListOrdersController.ts
@@ -0,0 +1,16 @@
+import { type Request, type Response } from 'express';
+import { container } from 'tsyringe';
+
+import { ListOrdersUseCase } from './ListOrdersUseCase';
+
+class ListOrdersController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const listOrdersUseCase = container.resolve(ListOrdersUseCase);
+
+    const orders = await listOrdersUseCase.execute();
+
+    return response.json(orders);
+  }
+}
+
+export { ListOrdersController };
