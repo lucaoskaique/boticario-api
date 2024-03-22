0b684a2eb9d942d853a44ce7bbd199212394761d
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-22 16:53:50 -0300

    Atualiza ICreateAddressDTO para permitir campos opcionais

--- src/modules/clients/dtos/ICreateAddressDTO.ts
+++ src/modules/clients/dtos/ICreateAddressDTO.ts
@@ -1,11 +1,11 @@
 interface ICreateAddressDTO {
-  cep: string;
-  street: string;
-  neighborhood: string;
-  city: string;
-  street_number: number;
-  complement: string;
-  uf: string;
+  cep?: string;
+  street?: string;
+  neighborhood?: string;
+  city?: string;
+  street_number?: number;
+  complement?: string;
+  uf?: string;
 }
 
 export type { ICreateAddressDTO };
