72bfabd899d5eab83cd116e07b4bab5d52ae8808
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-22 00:22:51 -0300

    Adiciona migração para criação da tabela de produtos

---
+++ src/typeorm/migrations/1711056044522-CreateProducts.ts
@@ -0,0 +1,73 @@
+import {
+  Table,
+  type MigrationInterface,
+  type QueryRunner,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateProducts1711056044522 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+          },
+          {
+            name: 'inventory_count',
+            type: 'integer',
+          },
+          {
+            name: 'category_id',
+            type: 'uuid',
+          },
+          {
+            name: 'image_url',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'products',
+      new TableForeignKey({
+        name: 'FK_category_id',
+        columnNames: ['category_id'],
+        referencedTableName: 'categories',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+        onUpdate: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('products');
+  }
+}
