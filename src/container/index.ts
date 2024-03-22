ae304b0b5db502f27045e22bfc2a568d2b676af3
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-22 17:38:56 -0300

    Adiciona repositório de pedidos ao contêiner de injeção de dependência

--- src/container/index.ts
+++ src/container/index.ts
@@ -3,8 +3,10 @@ import { ClientsRepository } from '@modules/clients/infra/typeorm/repositories/C
 import { type IAddressesRepository } from '@modules/clients/repositories/IAddressesRepository';
 import { type IClientsRepository } from '@modules/clients/repositories/IClientsRepository';
 import { CategoriesRepository } from '@modules/products/infra/typeorm/repositories/CategoriesRepository';
+import { OrdersRepository } from '@modules/products/infra/typeorm/repositories/OrdersRepository';
 import { ProductsRepository } from '@modules/products/infra/typeorm/repositories/ProductsRepository';
 import { type ICategoriesRepository } from '@modules/products/repositories/ICategoriesRepository';
+import { type IOrdersRepository } from '@modules/products/repositories/IOrdersRepository';
 import { type IProductsRepository } from '@modules/products/repositories/IProductsRepository';
 import { container } from 'tsyringe';
 
@@ -26,4 +28,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<IProductsRepository>(
   'ProductsRepository',
   ProductsRepository,
+);
+
+container.registerSingleton<IOrdersRepository>(
+  'OrdersRepository',
+  OrdersRepository,
 );
