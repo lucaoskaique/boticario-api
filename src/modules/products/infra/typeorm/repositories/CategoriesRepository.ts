386bf6336768c179865c3a02b87760cb7fefe9c8
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-22 01:45:50 -0300

    Adiciona repositório de categorias

---
+++ src/modules/products/infra/typeorm/repositories/CategoriesRepository.ts
@@ -0,0 +1,33 @@
+import { type ICreateCategoryDTO } from '@modules/products/dtos/ICreateCategoryDTO';
+import { type ICategoriesRepository } from '@modules/products/repositories/ICategoriesRepository';
+import { type Repository } from 'typeorm';
+import { AppDataSource } from 'typeorm/data-source';
+
+import { Category } from '../entities/Category';
+
+class CategoriesRepository implements ICategoriesRepository {
+  private repository: Repository<Category>;
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(Category);
+  }
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = this.repository.create({ name, description });
+
+    await this.repository.save(category);
+  }
+
+  async list(): Promise<Category[]> {
+    const categories = await this.repository.find();
+    return categories;
+  }
+
+  async findByName(name: string): Promise<Category | null> {
+    const category = this.repository.findOne({ where: { name } });
+
+    return await category;
+  }
+}
+
+export { CategoriesRepository };
