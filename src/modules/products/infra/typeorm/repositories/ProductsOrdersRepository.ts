f36d272ed6604e29be66e62cdd917311f0520c10
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-22 20:33:54 -0300

    Adiciona repositório ProductsOrdersRepository

---
+++ src/modules/products/infra/typeorm/repositories/ProductsOrdersRepository.ts
@@ -0,0 +1,58 @@
+import { type ICreateProductsOrdersDTO } from '@modules/products/dtos/ICreateProductsOrdersDTO';
+import { type IProductsOrdersRepository } from '@modules/products/repositories/IProductsOrdersRepository';
+import { type Repository } from 'typeorm';
+import { AppDataSource } from 'typeorm/data-source';
+
+import { ProductOrder } from '../entities/ProductOrder';
+
+class ProductsOrdersRepository implements IProductsOrdersRepository {
+  private repository: Repository<ProductOrder>;
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(ProductOrder);
+  }
+
+  public async create({
+    order_id,
+    product_price,
+    product_id,
+    product_count,
+  }: ICreateProductsOrdersDTO): Promise<ProductOrder> {
+    const productOrder = this.repository.create({
+      order_id,
+      product_price,
+      product_id,
+      product_count,
+    });
+
+    await this.repository.save(productOrder);
+
+    return productOrder;
+  }
+
+  public async findByOrderId(order_id: string): Promise<ProductOrder[]> {
+    const productsOrders = await this.repository.find({
+      where: { order_id },
+    });
+
+    return productsOrders;
+  }
+
+  public async findById(id: string): Promise<ProductOrder | null> {
+    const productOrder = await this.repository.findOne({ where: { id } });
+
+    return productOrder;
+  }
+
+  public async findByProductId(
+    product_id: string,
+  ): Promise<ProductOrder[] | null> {
+    const productsOrders = await this.repository.find({
+      where: { product_id },
+    });
+
+    return productsOrders;
+  }
+}
+
+export { ProductsOrdersRepository };
