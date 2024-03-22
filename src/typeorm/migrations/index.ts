e055d91087dd04649f65daca474850b7a1fed6a3
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-22 00:34:48 -0300

    Adicionadas migrações para criar pedidos e produtos

--- src/typeorm/migrations/index.ts
+++ src/typeorm/migrations/index.ts
@@ -1,9 +1,15 @@
 import { CreateCategories1710967133852 } from './1710984724753-CreateCategories';
 import { CreateAddresses1710985056783 } from './1710985056783-CreateAddresses';
 import { CreateClients1710986173055 } from './1710986173055-CreateClients';
+import { CreateOrders1711055979051 } from './1711055979051-CreateOrders';
+import { CreateProducts1711056044522 } from './1711056044522-CreateProducts';
+import { CreateProductsOrders1711078262488 } from './1711078262488-CreateProductsOrders';
 
 export {
   CreateClients1710986173055,
   CreateCategories1710967133852,
   CreateAddresses1710985056783,
+  CreateOrders1711055979051,
+  CreateProducts1711056044522,
+  CreateProductsOrders1711078262488,
 };
