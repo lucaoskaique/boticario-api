d3a783f8901b6c0dc08a01496bc48be956a0f5df
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-22 02:19:36 -0300

    Adiciona repositório de categorias ao container de injeção de dependências

--- src/container/index.ts
+++ src/container/index.ts
@@ -2,6 +2,8 @@ import { AddressesRepository } from '@modules/clients/infra/typeorm/repositories
 import { ClientsRepository } from '@modules/clients/infra/typeorm/repositories/ClientsRepository';
 import { type IAddressesRepository } from '@modules/clients/repositories/IAddressesRepository';
 import { type IClientsRepository } from '@modules/clients/repositories/IClientsRepository';
+import { CategoriesRepository } from '@modules/products/infra/typeorm/repositories/CategoriesRepository';
+import { type ICategoriesRepository } from '@modules/products/repositories/ICategoriesRepository';
 import { container } from 'tsyringe';
 
 container.registerSingleton<IClientsRepository>(
@@ -12,4 +14,9 @@ container.registerSingleton<IClientsRepository>(
 container.registerSingleton<IAddressesRepository>(
   'AddressesRepository',
   AddressesRepository,
+);
+
+container.registerSingleton<ICategoriesRepository>(
+  'CategoriesRepository',
+  CategoriesRepository,
 );
