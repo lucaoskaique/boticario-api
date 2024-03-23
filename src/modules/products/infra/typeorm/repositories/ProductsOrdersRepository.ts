a4adc2856bf33c36aa550924835bfe103f2a88df
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-22 22:02:21 -0300

    Atualizar importação de AppDataSource em ProductsOrdersRepository.ts

--- src/modules/products/infra/typeorm/repositories/ProductsOrdersRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsOrdersRepository.ts
@@ -1,7 +1,8 @@
 import { type ICreateProductsOrdersDTO } from '@modules/products/dtos/ICreateProductsOrdersDTO';
 import { type IProductsOrdersRepository } from '@modules/products/repositories/IProductsOrdersRepository';
 import { type Repository } from 'typeorm';
-import { AppDataSource } from 'typeorm/data-source';
+
+import { AppDataSource } from '@shared/infra/typeorm/data-source';
 
 import { ProductOrder } from '../entities/ProductOrder';
 
