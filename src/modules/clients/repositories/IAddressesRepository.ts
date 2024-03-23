feadcdb7b7fe936b61dcadbe9d74fad8b8626649
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-22 21:07:06 -0300

    Adiciona método findById ao repositório de endereços

--- src/modules/clients/repositories/IAddressesRepository.ts
+++ src/modules/clients/repositories/IAddressesRepository.ts
@@ -5,6 +5,7 @@ interface IAddressesRepository {
   create: (data: ICreateAddressDTO) => Promise<void>;
   list: () => Promise<Address[]> | [];
   update: (data: Address) => Promise<Address>;
+  findById: (id: string) => Promise<Address | null>;
 }
 
 export type { IAddressesRepository };
