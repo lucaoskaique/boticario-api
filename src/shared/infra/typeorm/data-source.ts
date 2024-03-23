4d2859791917e7a55a2fb922cfbece2298b4e64f
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-22 21:39:45 -0300

    Adiciona arquivo de configuração do banco de dados

---
+++ src/shared/infra/typeorm/data-source.ts
@@ -0,0 +1,31 @@
+import { DataSource } from 'typeorm';
+
+import {
+  CreateClients1710986173055,
+  CreateAddresses1710985056783,
+  CreateCategories1710967133852,
+  CreateOrders1711055979051,
+  CreateProducts1711056044522,
+  CreateProductsOrders1711078262488,
+} from '@shared/infra/typeorm/migrations';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'docker',
+  password: 'password',
+  database: 'boticario',
+  synchronize: true,
+  logging: false,
+  entities: ['src/modules/**/infra/typeorm/entities/*.ts'],
+  subscribers: [],
+  migrations: [
+    CreateClients1710986173055,
+    CreateAddresses1710985056783,
+    CreateCategories1710967133852,
+    CreateOrders1711055979051,
+    CreateProducts1711056044522,
+    CreateProductsOrders1711078262488,
+  ],
+});
