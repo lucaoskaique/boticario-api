ae1e4422f0dfa0c2638c6610d8579e692e85e6b5
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-23 21:57:51 -0300

    Atualiza IOrdersRepository para adicionar o método findLastOrderOfYear

--- src/modules/products/repositories/IOrdersRepository.ts
+++ src/modules/products/repositories/IOrdersRepository.ts
@@ -3,8 +3,9 @@ import { type Order } from '../infra/typeorm/entities/Order';
 
 interface IOrdersRepository {
   findById: (id: string) => Promise<Order | null>;
-  create: (data: ICreateOrderDTO) => Promise<void>;
+  create: (data: ICreateOrderDTO) => Promise<Order | undefined>;
   list: () => Promise<Order[]>;
+  findLastOrderOfYear: (year: number) => Promise<Order | null>;
 }
 
 export type { IOrdersRepository };
