e3e71dda9b8f4251036959187516be6d0fbe2fe8
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-24 16:22:08 -0300

    Adiciona repositório em memória para clientes

---
+++ src/modules/clients/repositories/in-memory/ClientsRepositoryInMemory.ts
@@ -0,0 +1,42 @@
+import { type ICreateClientDTO } from '@modules/clients/dtos/ICreateClientDTO';
+import { Client } from '@modules/clients/infra/typeorm/entities/Client';
+
+import { type IClientsRepository } from '../IClientsRepository';
+
+class ClientsRepositoryInMemory implements IClientsRepository {
+  private clients: Client[] = [];
+
+  async create(data: ICreateClientDTO): Promise<void> {
+    const client = new Client();
+    Object.assign(client, data);
+    this.clients.push(client);
+  }
+
+  async findByUsername(username: string): Promise<Client | null> {
+    const client = this.clients.find((client) => client.username === username);
+    return client ?? null;
+  }
+
+  async findById(id: string): Promise<Client | null> {
+    const client = this.clients.find((client) => client.id === id);
+    return client ?? null;
+  }
+
+  async list(): Promise<Client[]> {
+    return this.clients;
+  }
+
+  async update(data: Client): Promise<Client> {
+    const index = this.clients.findIndex((client) => client.id === data.id);
+    this.clients[index] = data;
+    return data;
+  }
+
+  async delete(id: string): Promise<boolean> {
+    const index = this.clients.findIndex((client) => client.id === id);
+    this.clients.splice(index, 1);
+    return true;
+  }
+}
+
+export { ClientsRepositoryInMemory };
