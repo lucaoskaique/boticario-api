0b1e67f43dd18ca67683f62db1e5dd9ef13d0f63
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-24 00:28:32 -0300

    Refatorar ListOrdersUseCase para retornar pedidos serializados

--- src/modules/products/useCases/listOrders/ListOrdersUseCase.ts
+++ src/modules/products/useCases/listOrders/ListOrdersUseCase.ts
@@ -1,7 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { type Order } from '@modules/products/infra/typeorm/entities/Order';
 import { IOrdersRepository } from '@modules/products/repositories/IOrdersRepository';
 import { inject, injectable } from 'tsyringe';
 
+type IAny = Record<string, any>;
+
+interface IResponse {
+  orders: Order[];
+  count: number;
+}
 @injectable()
 class ListOrdersUseCase {
   constructor(
@@ -9,10 +16,35 @@ class ListOrdersUseCase {
     private ordersRepository: IOrdersRepository,
   ) {}
 
-  async execute(): Promise<Order[]> {
-    const orders = await this.ordersRepository.list();
+  async execute(client_id: string): Promise<IResponse> {
+    const [orders, count] = await this.ordersRepository.findAndCount(client_id);
 
-    return orders;
+    const serializedOrders = orders.map((order) => {
+      const products = order?.productOrders?.map((productOrder) => {
+        const productCopy: IAny = { ...productOrder.product };
+
+        const productOrderCopy = { ...productOrder };
+        delete productOrderCopy.product;
+
+        productCopy.product_orders = productOrderCopy;
+
+        return productCopy;
+      });
+
+      const orderCopy: IAny = { ...order };
+      delete orderCopy.productOrders;
+      orderCopy.products = products;
+
+      orderCopy.createdAt = orderCopy.created_at;
+      delete orderCopy.created_at;
+
+      return orderCopy;
+    });
+
+    return {
+      orders: serializedOrders as Order[],
+      count,
+    };
   }
 }
 
