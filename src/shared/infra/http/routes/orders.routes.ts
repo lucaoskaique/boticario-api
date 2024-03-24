8dbdcb4eb862fe67e70ea83fb0feaa9467136d3c
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-24 15:30:03 -0300

    Adiciona rotas para listar pedidos por ID e listar todos os pedidos

--- src/shared/infra/http/routes/orders.routes.ts
+++ src/shared/infra/http/routes/orders.routes.ts
@@ -1,9 +1,15 @@
 import { CreateOrderController } from '@modules/products/useCases/createOrder/CreateOrderController';
+import { ListOrderByIdController } from '@modules/products/useCases/listOrderById/ListOrderByIdController';
+import { ListOrdersController } from '@modules/products/useCases/listOrders/ListOrdersController';
 import { Router } from 'express';
 
 const ordersRoutes = Router();
 const createOrderController = new CreateOrderController();
+const listOrderByIdController = new ListOrderByIdController();
+const listOrdersController = new ListOrdersController();
 
 ordersRoutes.post('/', createOrderController.handle);
+ordersRoutes.get('/', listOrdersController.handle);
+ordersRoutes.get('/:id', listOrderByIdController.handle);
 
 export { ordersRoutes };
