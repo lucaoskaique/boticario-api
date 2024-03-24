19385cb3a9fe869fae3721e927a06db74f443349
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-23 21:58:29 -0300

    Atualiza ICreateOrderDTO para adicionar status e número do pedido

--- src/modules/products/dtos/ICreateOrderDTO.ts
+++ src/modules/products/dtos/ICreateOrderDTO.ts
@@ -1,8 +1,11 @@
+import { type OrderStatus } from '../infra/typeorm/entities/Order';
+
 interface ICreateOrderDTO {
   amount: number;
   order_date: Date;
-  status: string;
+  status?: OrderStatus;
   client_id: string;
+  order_number?: string;
 }
 
 export type { ICreateOrderDTO };
