78991d5ad8cad79c8832d1831a737ad8802a764f
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-24 01:02:52 -0300

    Adiciona controladores para deletar e atualizar clientes

--- src/shared/infra/http/routes/clients.routes.ts
+++ src/shared/infra/http/routes/clients.routes.ts
@@ -1,13 +1,19 @@
 import { CreateClientController } from '@modules/clients/useCases/createClient/CreateClientController';
+import { DeleteClientController } from '@modules/clients/useCases/deleteClient/deleteClientController';
 import { ListClientsController } from '@modules/clients/useCases/listClients/ListClientsController';
+import { UpdateClientController } from '@modules/clients/useCases/updateClient/UpdateClientController';
 import { Router } from 'express';
 
 const clientsRoutes = Router();
 
 const createClientController = new CreateClientController();
 const listClientsController = new ListClientsController();
+const deleteClientController = new DeleteClientController();
+const updateClientController = new UpdateClientController();
 
 clientsRoutes.post('/', createClientController.handle);
 clientsRoutes.get('/list', listClientsController.handle);
+clientsRoutes.delete('/:id', deleteClientController.handle);
+clientsRoutes.patch('/:id', updateClientController.handle);
 
 export { clientsRoutes };
