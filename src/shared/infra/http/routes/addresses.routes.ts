7b0f1e957ec07ace7dd5fd9959ee986df5478acd
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-24 01:09:09 -0300

    Adiciona controladores de exclusão e atualização de endereço

--- src/shared/infra/http/routes/addresses.routes.ts
+++ src/shared/infra/http/routes/addresses.routes.ts
@@ -1,13 +1,19 @@
 import { CreateAddressController } from '@modules/clients/useCases/createAddress/CreateAddressController';
+import { DeleteAddressController } from '@modules/clients/useCases/deleteAddress/deleteAdressController';
 import { ListAddressController } from '@modules/clients/useCases/listAddress/ListAddressController';
+import { UpdateAddressController } from '@modules/clients/useCases/updateAddress/UpdateAddressController';
 import { Router } from 'express';
 
 const addressesRoutes = Router();
 
 const createAddressController = new CreateAddressController();
 const listAddressController = new ListAddressController();
+const deleteAddressController = new DeleteAddressController();
+const updateAddressController = new UpdateAddressController();
 
 addressesRoutes.post('/', createAddressController.handle);
 addressesRoutes.get('/list', listAddressController.handle);
+addressesRoutes.delete('/:id', deleteAddressController.handle);
+addressesRoutes.patch('/:id', updateAddressController.handle);
 
 export { addressesRoutes };
