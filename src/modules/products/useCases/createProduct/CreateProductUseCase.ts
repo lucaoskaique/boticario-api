4a43089a7db57fe3fbefa96488dc46831fca1047
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-24 15:33:21 -0300

    Adiciona validação de categoria e tratamento de erro na criação de produto

--- src/modules/products/useCases/createProduct/CreateProductUseCase.ts
+++ src/modules/products/useCases/createProduct/CreateProductUseCase.ts
@@ -1,11 +1,16 @@
 import { type ICreateProductDTO } from '@modules/products/dtos/ICreateProductDTO';
+import { ICategoriesRepository } from '@modules/products/repositories/ICategoriesRepository';
 import { IProductsRepository } from '@modules/products/repositories/IProductsRepository';
 import { inject } from 'tsyringe';
+
+import { AppError } from '@shared/errors/AppError';
 
 class CreateProductUseCase {
   constructor(
     @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CategoriesRepository')
+    private categoriesRepository: ICategoriesRepository,
   ) {}
 
   async execute({
@@ -19,7 +24,13 @@ class CreateProductUseCase {
     const productAlreadyExists = await this.productsRepository.findByName(name);
 
     if (productAlreadyExists) {
-      throw new Error('Product already exists');
+      throw new AppError('Product already exists');
+    }
+
+    const category = await this.categoriesRepository.findById(category_id);
+
+    if (!category) {
+      throw new AppError('Category does not exist');
     }
 
     await this.productsRepository.create({
