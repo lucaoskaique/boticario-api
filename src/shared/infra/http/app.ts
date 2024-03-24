455efbc1648e00a36c49058b60da6aaee5bb9299
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-24 19:22:37 -0300

    Adiciona configuração do Swagger e rota para arquivos estáticos

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,9 +1,12 @@
 import cors from 'cors';
 import express from 'express';
-
 import 'dotenv/config';
+import { resolve } from 'path';
+import swaggerUi from 'swagger-ui-express';
+
 import { routes } from '@shared/infra/http/routes';
 
+import swaggerFile from '../../../swagger.json';
 import 'reflect-metadata';
 import 'express-async-errors';
 import '@shared/container';
@@ -14,6 +17,10 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use('/files', express.static(resolve(__dirname, '..', '..', '..', 'tmp')));
 
 app.use(routes);
 
