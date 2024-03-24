9f174be75f7d5858a03319dc317a92e5dc9e32a4
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-24 02:16:03 -0300

    Adiciona controladores para deletar e atualizar categorias

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -1,16 +1,22 @@
 import { CreateCategoryController } from '@modules/products/useCases/createCategory/CreateCategoryController';
+import { DeleteCategoryController } from '@modules/products/useCases/deleteCategory/DeleteCategoryController';
 import { ListCategoryController } from '@modules/products/useCases/listCategories/ListCategoryController';
+import { ListCategoryByIdController } from '@modules/products/useCases/listCategoryById/ListCategoryByIdController';
+import { UpdateCategoryController } from '@modules/products/useCases/updateCategory/updateCategoryController';
 import { Router } from 'express';
 
 const categoriesRoutes = Router();
 
 const createCategoryController = new CreateCategoryController();
 const listCategoryController = new ListCategoryController();
+const listCategoryByIdController = new ListCategoryByIdController();
+const updateCategoryController = new UpdateCategoryController();
+const deleteCategoryController = new DeleteCategoryController();
 
 categoriesRoutes.post('/', createCategoryController.handle);
 categoriesRoutes.get('/list', listCategoryController.handle);
-categoriesRoutes.get('/:id', listCategoryController.handle);
-categoriesRoutes.delete('/:id', listCategoryController.handle);
-categoriesRoutes.patch('/:id', listCategoryController.handle);
+categoriesRoutes.get('/:id', listCategoryByIdController.handle);
+categoriesRoutes.delete('/:id', deleteCategoryController.handle);
+categoriesRoutes.patch('/:id', updateCategoryController.handle);
 
 export { categoriesRoutes };
