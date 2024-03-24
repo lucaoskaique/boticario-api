9b1f9df92d2d524ed5a795f7817339deefd115d6
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-24 18:28:43 -0300

    Atualizar tipo de retorno do método create em IAddressesRepository

--- src/modules/clients/repositories/IAddressesRepository.ts
+++ src/modules/clients/repositories/IAddressesRepository.ts
@@ -2,7 +2,7 @@ import { type ICreateAddressDTO } from '../dtos/ICreateAddressDTO';
 import { type Address } from '../infra/typeorm/entities/Address';
 
 interface IAddressesRepository {
-  create: (data: ICreateAddressDTO) => Promise<void>;
+  create: (data: ICreateAddressDTO) => Promise<Address>;
   list: () => Promise<Address[]> | [];
   update: (data: Address) => Promise<Address>;
   findById: (id: string) => Promise<Address | null>;
