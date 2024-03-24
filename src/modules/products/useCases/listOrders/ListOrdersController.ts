c3ab5d1742b86c543a15cfd645c92050f4b4bb2a
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-24 00:28:35 -0300

    Atualiza o controlador ListOrdersController para receber o parâmetro client_id e passá-lo para o caso de uso ListOrdersUseCase.execute()

--- src/modules/products/useCases/listOrders/ListOrdersController.ts
+++ src/modules/products/useCases/listOrders/ListOrdersController.ts
@@ -1,13 +1,20 @@
 import { type Request, type Response } from 'express';
 import { container } from 'tsyringe';
+import { z } from 'zod';
 
 import { ListOrdersUseCase } from './ListOrdersUseCase';
 
 class ListOrdersController {
   async handle(request: Request, response: Response): Promise<Response> {
+    const { client_id } = z
+      .object({
+        client_id: z.string(),
+      })
+      .parse(request.params);
+
     const listOrdersUseCase = container.resolve(ListOrdersUseCase);
 
-    const orders = await listOrdersUseCase.execute();
+    const orders = await listOrdersUseCase.execute(client_id);
 
     return response.json(orders);
   }
