1899e54f313e695a7ac317b691d8e0afd9d20bec
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-24 23:36:02 -0300

    Atualiza controlador de atualização de cliente

--- src/modules/clients/useCases/updateClient/UpdateClientController.ts
+++ src/modules/clients/useCases/updateClient/UpdateClientController.ts
@@ -7,31 +7,34 @@ import { UpdateClientUseCase } from './UpdateClientUseCase';
 class UpdateClientController {
   async handle(request: Request, response: Response): Promise<Response> {
     const updateClientBodySchema = z.object({
-      name: z.string(),
-      email: z.string(),
-      password: z.string(),
-      phone: z.string(),
-      birth_date: z.date(),
-      address_id: z.string(),
+      name: z.string().optional(),
+      email: z.string().optional(),
+      phone: z.string().optional(),
+      birth_date: z.coerce.date().optional(),
+      address_id: z.string().optional(),
     });
 
-    const { name, email, password, phone, birth_date, address_id } =
+    const { name, email, phone, birth_date, address_id } =
       updateClientBodySchema.parse(request.body);
 
     const id = z.string().parse(request.params.id);
+    let client;
+    try {
+      const updateClientUseCase = container.resolve(UpdateClientUseCase);
 
-    const updateClientUseCase = container.resolve(UpdateClientUseCase);
-
-    const client = await updateClientUseCase.execute({
-      id,
-      name,
-      email,
-      password,
-      phone,
-      birth_date,
-      address_id,
-    });
-
+      client = await updateClientUseCase.execute({
+        id,
+        name,
+        email,
+        phone,
+        birth_date,
+        address_id,
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        return response.status(400).json({ error: error.message });
+      }
+    }
     return response.json(client);
   }
 }
