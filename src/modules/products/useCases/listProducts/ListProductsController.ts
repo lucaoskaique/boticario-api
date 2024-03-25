994aa2231c41c236150e5ccecf031195b9077557
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-25 00:24:23 -0300

    Corrige manipulação de erros no controlador de listagem de produtos

--- src/modules/products/useCases/listProducts/ListProductsController.ts
+++ src/modules/products/useCases/listProducts/ListProductsController.ts
@@ -1,27 +1,30 @@
 import { type Request, type Response } from 'express';
 import { container } from 'tsyringe';
 
+import { AppError } from '@shared/errors/AppError';
+
 import { ListProductsUseCase } from './ListProductsUseCase';
 
 class ListProductsController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const category_id = request.query.category_id
-      ? String(request.query.category_id)
-      : undefined;
-
     const limit = request.query.limit ? Number(request.query.limit) : undefined;
     const offset = request.query.offset
       ? Number(request.query.offset)
       : undefined;
 
-    const listProductsUseCase = container.resolve(ListProductsUseCase);
+    let products;
+    try {
+      const listProductsUseCase = container.resolve(ListProductsUseCase);
 
-    const products = await listProductsUseCase.execute({
-      category_id,
-      limit,
-      offset,
-    });
-
+      products = await listProductsUseCase.execute({
+        limit,
+        offset,
+      });
+    } catch (error) {
+      if (error instanceof AppError) {
+        return response.status(400).json({ error: error.message });
+      }
+    }
     return response.json(products);
   }
 }
