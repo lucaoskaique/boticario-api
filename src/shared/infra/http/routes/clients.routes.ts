baec5ce12e4a526005f854ed7f0f9fa89e432969
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-24 23:35:47 -0300

    Adiciona rota para listar cliente por ID

--- src/shared/infra/http/routes/clients.routes.ts
+++ src/shared/infra/http/routes/clients.routes.ts
@@ -1,5 +1,6 @@
 import { CreateClientController } from '@modules/clients/useCases/createClient/CreateClientController';
 import { DeleteClientController } from '@modules/clients/useCases/deleteClient/deleteClientController';
+import { ListClientByIdController } from '@modules/clients/useCases/listClientById/ListClientByIdController';
 import { ListClientsController } from '@modules/clients/useCases/listClients/ListClientsController';
 import { UpdateClientController } from '@modules/clients/useCases/updateClient/UpdateClientController';
 import { Router } from 'express';
@@ -8,11 +9,13 @@ const clientsRoutes = Router();
 
 const createClientController = new CreateClientController();
 const listClientsController = new ListClientsController();
+const listClientByIdController = new ListClientByIdController();
 const deleteClientController = new DeleteClientController();
 const updateClientController = new UpdateClientController();
 
 clientsRoutes.post('/', createClientController.handle);
 clientsRoutes.get('/list', listClientsController.handle);
+clientsRoutes.get('/:id', listClientByIdController.handle);
 clientsRoutes.delete('/:id', deleteClientController.handle);
 clientsRoutes.patch('/:id', updateClientController.handle);
 
